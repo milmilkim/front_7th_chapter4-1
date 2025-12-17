8b297ca35dd37a20019b9fb55687e8e74b3e1bbd
sori kim <soundkim@gachon.ac.kr>
2025-12-17 23:15:33 +0900

    feat: 라우터

--- packages/vanilla/src/router/router.js
+++ packages/vanilla/src/router/router.js
@@ -2,4 +2,45 @@
 import { Router } from "../lib";
 import { BASE_URL } from "../constants.js";
 
-export const router = new Router(BASE_URL);
+const clientRouter = typeof window !== "undefined" ? new Router(BASE_URL) : null;
+
+const getRouterInstance = () => {
+  if (clientRouter) return clientRouter;
+  if (typeof global !== "undefined" && global.router) return global.router;
+  return { params: {}, query: {} };
+};
+
+export const router = {
+  get params() {
+    return getRouterInstance().params;
+  },
+  get query() {
+    return getRouterInstance().query;
+  },
+  set query(newQuery) {
+    if (clientRouter) {
+      clientRouter.query = newQuery;
+    }
+  },
+  get route() {
+    return getRouterInstance().route;
+  },
+  get target() {
+    return getRouterInstance().target;
+  },
+  get baseUrl() {
+    return getRouterInstance().baseUrl || BASE_URL;
+  },
+  addRoute(...args) {
+    return clientRouter?.addRoute(...args);
+  },
+  push(...args) {
+    return clientRouter?.push(...args);
+  },
+  start() {
+    return clientRouter?.start();
+  },
+  subscribe(...args) {
+    return clientRouter?.subscribe(...args);
+  },
+};
