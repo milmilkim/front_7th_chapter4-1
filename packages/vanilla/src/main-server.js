3205cce5f8fb2a1123f55a3df445773125302600
sori kim <soundkim@gachon.ac.kr>
2025-12-17 21:32:55 +0900

    Update main-server.js

--- packages/vanilla/src/main-server.js
+++ packages/vanilla/src/main-server.js
@@ -1,4 +1,11 @@
 export const render = async (url, query) => {
   console.log({ url, query });
-  return "";
+
+  // 무슨 일이 일어남
+
+  return {
+    head: "",
+    html: "<h1>hello world~!!</h1>",
+    initialDataScript: "",
+  };
 };
